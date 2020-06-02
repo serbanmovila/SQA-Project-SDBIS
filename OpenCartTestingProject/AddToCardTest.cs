05212336737279d5c718500df477fd69a2791784
vladislava-st <vladislava.st.v@gmail.com>
2020-06-03 00:26:19 +0300

    add to cart from product page

--- OpenCartTestingProject/AddToCardTest.cs
+++ OpenCartTestingProject/AddToCardTest.cs
@@ -40,7 +40,9 @@ namespace OpenCartTestingProject
         public void AddToCard()
         {
             productPage.AddToCart();
+
             String expectedResult = "Success: You have added " + productPageBO.ProductName + " to your shopping cart!\r\n×";
+            
             Assert.AreEqual(expectedResult, productPage.SuccessfullyUpdatedText);
         }
 
